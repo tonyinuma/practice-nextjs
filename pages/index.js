99f831f03e254c6832fb1125ff1ad7325580e853
tony inuma <tinuma@afe.pe>
2020-10-22 12:10:09 -0500

    feat: user fetch api request

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Container from "../components/container";
 import React from "react";
+import fetch from 'isomorphic-fetch';
+import Users from "../components/users";
 
-const Index = () => {
+
+const Index = (props) => {
     return (
         <Container>
             <h1>Hello world</h1>
+            <Users users={props.users}/>
         </Container>
     )
 }
 
-Index.getInitialProps = (context) =>{
-
+Index.getInitialProps = async (context) => {
+    const response = await fetch(`https://reqres.in/api/users`);
+    const resJSON = await response.json();
+    return {users:resJSON.data}
 }
 
 export default Index
