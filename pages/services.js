6defdebd2f47cbd4ee7d5234487c12570b2b100a
tony inuma <tinuma@afe.pe>
2020-10-22 09:16:49 -0500

    first commit

---
+++ pages/services.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/conatiner";
+import Container from "../components/conatiner";
+import React from "react";
+
+const About = () => {
+    return <Container>
+        <h1>Services</h1>
+    </Container>
+}
+
+export default About
