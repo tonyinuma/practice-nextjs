f82ca3714048f30329ee761f6a34eed7ee8b5d0f
TonyID <tony.inuma@gmail.com>
2020-10-22 10:25:40 -0500

    add: navbar items

--- components/navigation.js
+++ components/navigation.js
@@ -3,23 +3,29 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <ul>
-            <li>
-                <Link href="/">
-                    <a>Index</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/services">
-                    <a>Services</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/about">
-                    <a>About</a>
-                </Link>
-            </li>
-        </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand ml-4" href="/">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/services">Servicios</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/cotizacion">Cotizacion</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about">About</a>
+                    </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0 mr-4">
+                    <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+                    <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
     )
 }
 
