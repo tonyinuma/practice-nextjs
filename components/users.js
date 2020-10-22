27da76d657f94f0cecff6df1206637df91847e11
TonyID <tony.inuma@gmail.com>
2020-10-22 13:05:21 -0500

    Feat: link to user detail

--- components/users.js
+++ components/users.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Router from "next/router";
 
 const Users = (props) => {
     console.log(props);
@@ -6,7 +7,8 @@ const Users = (props) => {
         <ul className="list-group">
             {
                 props.users.map(user => (
-                    <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-lg-center" key={user.id}>
+                    <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-lg-center"
+                        key={user.id} onClick={e =>Router.push('/users/[id]',`/users/${user.id}`)}>
                         <div>
                             <h5>{user.first_name}, {user.last_name}</h5>
                             <p>Email: {user.email}</p>
