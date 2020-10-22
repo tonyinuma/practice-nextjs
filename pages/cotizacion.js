0170129b6edb77291c8ddb28572fcd78ac2c9564
tony inuma <tinuma@afe.pe>
2020-10-22 10:25:08 -0500

    add: page cotizacion

---
+++ pages/cotizacion.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from "../components/container";
+import Head from "next/head";
+
+const Cotizacion = () => {
+    return <Container>
+        <Head>
+            <title>Cotizacion</title>
+        </Head>
+        <div className="container">
+            <h1>Cotizacion</h1>
+        </div>
+    </Container>
+}
+
+export default Cotizacion
